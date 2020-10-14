301634a1feba929765ec286efd8d29a67685b2c4
Abdennour Hachemi <nouri89@hotmail.com>
2020-10-14 22:33:00 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 const Header = () => {
+	function handelClick() {
+		
+	}
 	return (
 		<div style={{ width: "100%", padding: "2% 0  2% 10%", display: "flex" }}>
-			<h3>Where in the world?</h3>
-			<h6 style={{ paddingLeft: "60%" }}> Night Mode</h6>
+			<h3 style={{fontWeight:"bolder"}}>Where in the world?</h3>
+			<div onClick={handelClick}
+				style={{
+					
+					display: "flex",
+					paddingLeft: "60%",
+				}}
+			>
+				<i
+					className="fa fa-moon-o"
+					style={{ fontSize: "30px" ,paddingRight:"10px" }}
+				></i>
+				<h4 style={{fontWeight:"bold"}} >
+				Dark Mode
+				</h4>
+			</div>
 		</div>
 	);
 };
