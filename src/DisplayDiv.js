6142d9b1bd07d52e223cb1a12a129c91a38c328a
Abdennour Hachemi <nouri89@hotmail.com>
2020-10-16 05:32:04 +0100

    Update DisplayDiv.js

--- src/DisplayDiv.js
+++ src/DisplayDiv.js
@@ -2,21 +2,35 @@ import React, { useState } from "react";
 import countriesAll from "./countriesAll.json";
 import CountryCard from "./CountryCard";
 import SearchDiv from "./SearchDiv";
-
+import SingleCountryDisplay from "./SingleCountryDisplay";
 
 const DisplayDiv = () => {
 	const data = countriesAll;
-	
+	const [singleCountryMode, setSingleCountryMode] = useState(false);
 	const [searchValue, setSearchValue] = useState("");
 	const [regionSearch, setRegionSearch] = useState("");
+	const [countries, setCountries] = useState([]);
+	console.log(singleCountryMode);
 
-	console.log(searchValue);
+	function handleCountryClick(countryName) {
+		setSingleCountryMode(true);
+		setCountries(countriesAll.find((item) => item.name === countryName));
+	}
+
 	function handelRegionSearchValue(event) {
 		setSearchValue(event.target.value);
 	}
 
 	function handelRegionSearch(event) {
 		setRegionSearch(event.target.value);
+	}
+	function handleBackButtonClick() {
+		setSingleCountryMode(false);
+	}
+
+	function handleSingleCountryClick(countryName) {
+		setSingleCountryMode(true);
+		setCountries(countriesAll.find((item) => item.name === countryName));
 	}
 
 	let searchResult = data.filter((country) => {
@@ -33,28 +47,50 @@ const DisplayDiv = () => {
 			country.region.toLowerCase().includes(regionSearch.toLowerCase())
 		);
 	});
-
-	return (
-		<div>
-			<SearchDiv
-				handelRegionSearchValue={handelRegionSearchValue}
-				searchValue={searchValue}
-				handelRegionSearch={handelRegionSearch}
-			/>
-			<div className="row" style={{ backgroundColor: "#F3F3F3" }}>
-				{searchRegionResult.map((country, index) => (
-					<CountryCard
-						key={index}
-						flag={country.flag}
-						name={country.name}
-						population={country.population}
-						region={country.region}
-						capital={country.capital}
-					/>
-				))}
+	if (!singleCountryMode) {
+		return (
+			<div>
+				<SearchDiv
+					handelRegionSearchValue={handelRegionSearchValue}
+					searchValue={searchValue}
+					handelRegionSearch={handelRegionSearch}
+				/>
+				<div className="row" style={{ backgroundColor: "#F3F3F3" }}>
+					{searchRegionResult.map((country, index) => (
+						<CountryCard
+							key={index}
+							flag={country.flag}
+							name={country.name}
+							population={country.population}
+							region={country.region}
+							capital={country.capital}
+							handleCountryClick={handleCountryClick}
+						/>
+					))}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div>
+				<SingleCountryDisplay
+					key={countries.id}
+					flag={countries.flag}
+					name={countries.name}
+					population={countries.population}
+					region={countries.region}
+					capital={countries.capital}
+					nativeName={countries.nativeName}
+					topLevelDomain={countries.topLevelDomain}
+					currencies={countries.currencies}
+					languages={countries.languages}
+					borders={countries.borders}
+					handleBackButtonClick={handleBackButtonClick}
+					handleSingleCountryClick={handleSingleCountryClick}
+				/>
+			</div>
+		);
+	}
 };
 
 export default DisplayDiv;
