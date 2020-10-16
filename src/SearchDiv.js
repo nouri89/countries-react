cec225c241a757e01a10b68c7f084edfda7f1759
Abdennour Hachemi <nouri89@hotmail.com>
2020-10-16 05:34:25 +0100

    Update SearchDiv.js

--- src/SearchDiv.js
+++ src/SearchDiv.js
@@ -1,38 +1,35 @@
 import React from "react";
 
-
-
 const SearchDiv = ({
 	handelRegionSearchValue,
 	searchValue,
 	handelRegionSearch,
 }) => {
 	return (
-		<div
-			className="searchDiv"
-			style={{ display: "flex", padding: "3% 3%", backgroundColor: "#F3F3F3" }}
-		>
-			<input
-				className="searchInput"
-				type="text"
-				placeholder="&#61442; Search for Country..."
-				style={{ marginLeft: "4%" }}
-				value={searchValue} 
-				onChange={handelRegionSearchValue}
-				id="country"
-			></input>
+		
+			<div className="searchDiv">
+				<input
+					className="searchInput"
+					type="text"
+					placeholder="&#61442; Search for Country..."
+					style={{ marginLeft: "4%", fontSize: "1.5rem" }}
+					value={searchValue}
+					onChange={handelRegionSearchValue}
+					id="country"
+				></input>
 
-			<div style={{ paddingLeft: "70%" }}>
-				<select id="region" name="regions" onChange={handelRegionSearch}>
-					<option value="">Filter by Region</option>
-					<option value="Africa">Africa</option>
-					<option value="Americas">Americas</option>
-					<option value="Asia">Asia</option>
-					<option value="Europe">Europe</option>
-					<option value="Oceania">Oceania</option>
-				</select>
+				<div style={{ paddingLeft: "60%", fontSize: "1.5rem" }}>
+					<select id="region" name="regions" onChange={handelRegionSearch}>
+						<option value="">Filter by Region</option>
+						<option value="Africa">Africa</option>
+						<option value="Americas">Americas</option>
+						<option value="Asia">Asia</option>
+						<option value="Europe">Europe</option>
+						<option value="Oceania">Oceania</option>
+					</select>
+				</div>
 			</div>
-		</div>
+		
 	);
 };
 
